f063a792cbd9abc041e8b2bfacba9468fb1df932
sixzerexs <srvoplayer@gmail.com>
2024-06-27 08:58:02 +0500

    new func

--- bot1/handlers.py
+++ bot1/handlers.py
@@ -4,7 +4,7 @@ import io
 import secrets
 from PIL import Image, ImageFilter, ImageEnhance
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram import Router
 from dotenv import load_dotenv
 
@@ -18,30 +18,30 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f"Hello, {message.from_user.full_name}!")
+    await message.reply(f"Hello, @{message.from_user.username}! Just send a photo if you want to make it unique.\n\nCommands:\n1. /start\n2. /info\n\n*the bot may be delayed @uniqtbot")
     
 @router.message(Command('info'))
 async def cmd_info(message: Message):
-    await message.answer(f"UniqBot is bot for tiktok video uniqalazator \n\nHow it use? \n1. Select the command \"Uniquelize\"\n2. Send a img what u would to unique\n3. Save the image the bot sent you\n\n@uniqtbot")
-    print(img)
+    await message.answer(f"UniqBot is bot for tiktok video uniqalazator \n\nHow it use?\n1. Send me the image you want to make unique.\n2. Save the image the bot sent you\n3. Use\n\n@uniqtbot")
     
 @router.message()
 async def cmd(message: Message):
     if message.photo:
-        global img
         lastElement = len(message.photo) - 1
         fileid = message.photo[lastElement].file_id
         resp = requests.get(URI_PHOTO_INFO + fileid)
-        img_path = resp.json()['result']['file_path']
-        img = requests.get(URI_PHOTO_PATH + img_path)
+        img_uri_path = resp.json()['result']['file_path']
+        img = requests.get(URI_PHOTO_PATH + img_uri_path)
         img = Image.open(io.BytesIO(img.content))
         img = img.filter(ImageFilter.GaussianBlur(radius=1.05))
         img = ImageEnhance.Brightness(img).enhance(1.1)
         img = ImageEnhance.Contrast(img).enhance(1.1)
         img = ImageEnhance.Color(img).enhance(1.05)
         img = ImageEnhance.Sharpness(img).enhance(1.1)
-        if not os.path.exists('static'):
-            os.mkdir('static')
-        img.save(f'static/{secrets.token_hex(8)}.png', format='PNG')
+        if not os.path.exists('bot1/static'):
+            os.mkdir('bot1/static')
+        img_name = secrets.token_hex(8)
+        img.save(f'bot1/static/{img_name}.png', format='PNG')
+        await message.answer_photo(photo=FSInputFile(f'bot1/static/{img_name}.png'), caption='123')
     else:
         print(f'\"{message.text}\" from user - {message.from_user.id}')
