10a9c847c14754f91d551593546058f5f901a5cb
sixzerexs <srvoplayer@gmail.com>
2024-06-27 06:59:39 +0500

    add proccesing

--- bot1/handlers.py
+++ bot1/handlers.py
@@ -1,11 +1,12 @@
 import requests
 import os
+import io
+import secrets
+from PIL import Image, ImageFilter, ImageEnhance
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram import Router
 from dotenv import load_dotenv
-
-import keyboards as kb
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -17,22 +18,30 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f"Hello, {message.from_user.full_name}!", reply_markup=kb.main)
+    await message.reply(f"Hello, {message.from_user.full_name}!")
     
 @router.message(Command('info'))
 async def cmd_info(message: Message):
-    await message.answer(f"UniqBot is bot for tik tok video uniqalazator \n\nHow it use? \n1. Select the command \"Uniquelize\"\n2. Send a img what u would to unique\n3. Save the image the bot sent you\n\n@uniqtbot")
-    print(img_byte_code)
+    await message.answer(f"UniqBot is bot for tiktok video uniqalazator \n\nHow it use? \n1. Select the command \"Uniquelize\"\n2. Send a img what u would to unique\n3. Save the image the bot sent you\n\n@uniqtbot")
+    print(img)
     
 @router.message()
 async def cmd(message: Message):
     if message.photo:
-        global img_byte_code
+        global img
         lastElement = len(message.photo) - 1
         fileid = message.photo[lastElement].file_id
         resp = requests.get(URI_PHOTO_INFO + fileid)
         img_path = resp.json()['result']['file_path']
-        img_byte_code = (requests.get(URI_PHOTO_PATH + img_path)).content
+        img = requests.get(URI_PHOTO_PATH + img_path)
+        img = Image.open(io.BytesIO(img.content))
+        img = img.filter(ImageFilter.GaussianBlur(radius=1.05))
+        img = ImageEnhance.Brightness(img).enhance(1.1)
+        img = ImageEnhance.Contrast(img).enhance(1.1)
+        img = ImageEnhance.Color(img).enhance(1.05)
+        img = ImageEnhance.Sharpness(img).enhance(1.1)
+        if not os.path.exists('static'):
+            os.mkdir('static')
+        img.save(f'static/{secrets.token_hex(8)}.png', format='PNG')
     else:
-        print(f'\"{message.text}\" from user - {message.from_user.username}')
-        
+        print(f'\"{message.text}\" from user - {message.from_user.id}')
--- bot1/keyboards.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton as kbt
-
-main = ReplyKeyboardMarkup(keyboard=[[kbt(text='Uniqueize')],
-                                     [kbt(text='Home')]],
-                           resize_keyboard=True,
-                           input_field_placeholder='pic action...')
